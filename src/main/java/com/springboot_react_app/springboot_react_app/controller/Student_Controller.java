c881924d9400510fddd5355cee32908b2a644641
Ajay Kumar <ajaykumar286458@gmail.com>
2024-09-04 22:17:49 +0530

    cros origin set

--- src/main/java/com/springboot_react_app/springboot_react_app/controller/Student_Controller.java
+++ src/main/java/com/springboot_react_app/springboot_react_app/controller/Student_Controller.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/v1/student")
 public class Student_Controller {
 
